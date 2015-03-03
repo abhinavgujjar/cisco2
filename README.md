e476fd4c0f88bb7ea86a8757904046e1971e0c4f
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-03-03 12:01:55 +0530

    plknr

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Resources
 
 - [mongoose](http://cesanta.com/mongoose.shtml) is a lightweight web server. Serves up static filese
 
+- plnkr for directives - http://plnkr.co/edit/037865?p=preview
+
 Questions
 ----------------------
 
--- app/app.js
+++ app/app.js
@@ -1,21 +1,9 @@
 //declared a module
 var app = angular.module('myApp', []);
 
-function mcontroller($scope, $rootScope) {
-	$scope.greeting = 'Good Afternoon, Cisco!!';
-}
-
 //rerference a module
-app.controller('mainController', mcontroller);
-
-app.controller('nestedController',
-	function($scope, $rootScope) {
-		$rootScope.greeting = "OLA!";
+app.controller('mainController',function($scope, $rootScope) {
+		
+		$scope.greeting = 'Good Afternoon, Cisco!!';
 	}
-)
-
-app.controller('secondController',
-	function($scope) {
-		//$scope.greeting = "I am second!";
-	}
-)
+);
