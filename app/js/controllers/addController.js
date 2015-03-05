aee839be0490fe2907328e92456144a4d193a702
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-03-05 11:19:06 +0530

    hote images

--- app/js/controllers/addController.js
+++ app/js/controllers/addController.js
@@ -2,6 +2,27 @@ app.controller('addController', ["$scope", "uiconfig", "hotelsData", function($s
 
 	$scope.descLimit = uiconfig.descLimit;
 
+	var images = ["http://i.imgur.com/dmlhhkU.jpg",
+		"http://i.imgur.com/S54M0bj.jpg",
+		"http://i.imgur.com/hZg00lq.jpg",
+		"http://i.imgur.com/1jD7lgN.jpg",
+		"http://i.imgur.com/U0KoJxh.jpg",
+		"http://i.imgur.com/amBtxWl.jpg",
+		"http://i.imgur.com/QhVORNH.jpg",
+		"http://i.imgur.com/QhVORNH.jpg",
+		"http://i.imgur.com/BIBuk43.jpg",
+		"http://i.imgur.com/QezaFg8.jpg",
+		"http://i.imgur.com/mOja8KJ.jpg",
+		"http://i.imgur.com/7lSnfvh.jpg",
+		"http://i.imgur.com/Fq4cFeQ.jpg",
+		"http://i.imgur.com/3P9IDTF.jpg",
+		"http://i.imgur.com/QhNZdjf.jpg",
+		"http://i.imgur.com/o3sfWer.jpg",
+		"http://i.imgur.com/G9qKSJU.jpg",
+		"http://i.imgur.com/FfNahV4.jpg",
+		"http://i.imgur.com/OGLIG3N.jpg"
+	];
+
 	$scope.addHotel = function(hotel) {
 
 		if ($scope.hotelForm.$valid) {
